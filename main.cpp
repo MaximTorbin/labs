eb18083995e50b19f8a8f983dadc33c826cfd224
Max <132104744+MaximTorbin@users.noreply.github.com>
2024-09-20 23:37:54 +0200

    ADD lab 4,5 to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "RU");
-   /* setlocale(LC_ALL, "RU");
+   /* 2)setlocale(LC_ALL, "RU");
     cout << "Введитe радиус круга:\n";
    float r;
     cin >> r;
@@ -12,11 +12,49 @@ int main()
      int x, i;
     cout << "Введите число и бит:";
     cin >>x>> i;
-   /*a = 15;
+   /* 3.1)a = 15;
     mask = ~(1 << 3);
-    cout << (a&mask);*/
+    cout << (a&mask);
+
+    3.2)
     i = int(pow(2, i));
-    cout <<( x |i);
+    cout <<( x |i);*/
+
+	/*4)
+	unsigned int a;
+
+	cout << "введите ошибку:" << std::endl;
+	std::cin >> a;
+
+	switch (a) {
+	case 1:
+		std::cout << "ßáëîęî";
+		break;
+	case 2:
+		std::cout << "Ăđóřŕ";
+		break;
+	case 3:
+		std::cout << "Áŕíŕí";
+		break;
+	case 4:
+		std::cout << "Ěŕíäŕđčí";
+		break;
+	case 5:
+		std::cout << "Ŕđáóç";
+		break;
+	default:
+		std::cout << "неизвестная ошибка";
+		break;
+	}
 
     return 0;
 }
+*/
+	/*5)int num, tmp, sum = 0; cin >> num;
+	while (num)
+	{
+		tmp = num % 10;
+		if (tmp % 2 == 0) sum += tmp;
+		num /= 10;
+	}
+	cout << sum << endl;/*
