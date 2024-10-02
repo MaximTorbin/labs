6a8156758d860608b890eca6397ae369483c7bcd
Max <132104744+MaximTorbin@users.noreply.github.com>
2024-10-02 22:45:41 +0200

    lab6.cpp

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-bool simp(int x){
-    for(int i=2;i<(sqrt(x)+1);i++){
-        if (x % i == 0) {
-            return true;
-        }
-    }
-    if (x <= 0) {
-        return true;
-    }
-}
-//
-//int main(){
+// bool simp(int x)
+// {
+//     for(int i=2;i<(sqrt(x)+1);i++){
+//         if (x % i == 0) {
+//             return true;
+//         }
+//     }
+//     if (x <= 0) {
+//         return true;
+//     }
+// }
+// //№1
+// int main(){
 //    setlocale(LC_ALL, "Russian");
 //    cout<<"Введите длинну массива:\n";
 //    int l;
@@ -23,7 +24,9 @@ bool simp(int x){
 //        cin>>ar[i];
 //        if (simp(ar[i]) == 0) {
 //            fl = 1;
+//            break;
 //        }
+//    delete [] ar;
 //    }
 //    if (fl==0) {
 //        sort(ar, ar + l);
@@ -34,120 +37,114 @@ bool simp(int x){
 //    }
 //    else cout << "В массиве есть простой элемент";
 //    return 0;
-//    
-//}
+   
+// }
 
-
-
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//struct NumberProperties {
-//    int number;
-//    int digitSum;
-//    int digitProduct;
-//
-//    NumberProperties(int num) : number(num), digitSum(0), digitProduct(1) {
-//        int temp = num;
-//        while (temp > 0) {
-//            int digit = temp % 10;
-//            digitSum += digit;
-//            digitProduct *= digit;
-//            temp /= 10;
-//        }
-//    }
-//};
-//
-//// Компаратор для сортировки
-//bool compare(const NumberProperties& a, const NumberProperties& b) {
-//    if (a.digitSum != b.digitSum) {
-//        return a.digitSum < b.digitSum;
-//    }
-//    if (a.digitProduct != b.digitProduct) {
-//        return a.digitProduct < b.digitProduct;
-//    }
-//    return a.number < b.number;
-//}
-//
-//int main() {
-//    setlocale(LC_ALL, "Russian");
-//    int n;
-//
-//    std::cout << "Введите количество натуральных чисел (n <= 1000): ";
-//    std::cin >> n;
-//
-//    if (n <= 0 || n > 1000) {
-//        std::cout << "Ошибка: n должно быть в пределах от 1 до 1000." << std::endl;
-//        return 1;
-//    }
-//
-//    std::vector<NumberProperties> numbers;
-//
-//    // Ввод чисел
-//    std::cout << "Введите " << n << " натуральных чисел:" << std::endl;
-//    for (int i = 0; i < n; ++i) {
-//        int num;
-//        std::cin >> num;
-//
-//        // Проверка на натуральность
-//        if (num <= 0) {
-//            std::cout << "Ошибка: число должно быть натуральным." << std::endl;
-//            return 1;
-//        }
-//
-//        numbers.emplace_back(num); // Создаем объект и добавляем в вектор
-//    }
-//
-//    // Сортировка чисел
-//    std::sort(numbers.begin(), numbers.end(), compare);
-//
-//    // Вывод отсортированных чисел
-//    std::cout << "Отсортированные числа:" << std::endl;
-//    for (const auto& numProp : numbers) {
-//        std::cout << numProp.number << std::endl;
-//    }
-//
-//    return 0; // Успешное завершение программы
-//}
-int main() {
-    setlocale(LC_ALL, "Russian");
-
-    int m, n, pr, prm=0, str=0,a;
-    cout << "Количество строк:";
-    cin >> m;
-    cout << "Количество столбцов:";
-    cin >>n;
-    int** matrix = new int* [m];
-    for (int i = 0; i < m; i++) {
-        matrix[i] = new int[n];
+//№2
+int sumD(int a){
+	int sum=0;
+    while(a)
+    {
+        sum+=a%10;
+        a/=10;
     }
-    for (int i = 0; i < m; i++) {
-        pr = 1;
-        for (int j=0; j < n; j++) {
-            cout << "Введите элемент " << i+1 << " строки " << j+1 << " столбца:";
-            cin >> matrix[i][j];
-            pr = pr * matrix[i][j];
-            if (pr > prm) {
-                prm = pr;
-                str = i;
-            }
-        }
-    }
-    for (int i = 0; i < n;i++) {
-        matrix[str][i] = prm;
-    }
-
-    cout << "Максимальное произведение:" << prm << endl;
-    cout << "Матрица:";
-
-    for (int i = 0; i < m; i++) {
-        cout << endl;
-        for (int j = 0; j < n; j++) {
-            cout << matrix[i][j]<<" ";
-
-        }
-    }
-
-    return 0;
+	return sum;
 }
+
+int mulD(int a)
+{
+    int mul=1;
+    while(a)
+    {
+        mul*=a%10;
+        a/=10;
+    }
+    return mul;
+}
+
+ 
+bool sor(int n1,int n2)
+{
+    if (sumD(n1)>sumD(n2))
+    return true;
+    if (sumD(n1)<sumD(n2))
+    return false;
+
+    if (mulD(n1)>mulD(n2))
+    return true;
+    if (mulD(n1)<mulD(n2))
+    return false;
+    
+    if (n1>n2)
+    return true;
+    return false;
+}
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int a,b,n;
+	cout<<"Введите длинну последовательности:\n";
+    cin>>n;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++)
+    {
+		cout<<"Введите"<<i+1<< " элемент";
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if (sor(arr[i],arr[j]))
+            swap(arr[i],arr[j]);
+        }
+    }
+	cout<< "Отсортированный массив:\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    delete [] arr;
+}
+
+//№3
+// int main() {
+//     setlocale(LC_ALL, "Russian");
+//     int m, n, pr, prm=INT_MIN, str=0,a;
+//     cout << "Количество строк:";
+//     cin >> m;
+//     cout << "Количество столбцов:";
+//     cin >>n;
+//     int** matrix = new int* [m];
+//     for (int i = 0; i < m; i++) {
+//         matrix[i] = new int[n];
+//     }
+//     for (int i = 0; i < m; i++) {
+//         pr = 1;
+//         for (int j=0; j < n; j++) {
+//             cout << "Введите элемент " << i+1 << " строки " << j+1 << " столбца:";
+//             cin >> matrix[i][j];
+//             pr = pr * matrix[i][j];
+//             if (pr > prm) {
+//                 prm = pr;
+//                 str = i;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < n;i++) {
+//         matrix[str][i] = prm;
+//     }
+
+//     cout << "Максимальное произведение:" << prm << endl;
+//     cout << "Матрица:";
+
+//     for (int i = 0; i < m; i++) {
+//         cout << endl;
+//         for (int j = 0; j < n; j++) {
+//             cout << matrix[i][j]<<" ";
+
+//         }
+//     }
+
+//     delete [] matrix;
+// }
